b1031d29f34e4cd5ba167f55a64d92be8c181794
rafael.tein <rafael.tein@visaogrupo.com.br>
2025-01-28 12:00:41 -0300

    ajuste na confiema token

--- app/src/main/java/br/com/visaogrupo/tudofarmarep/Repository/Model/Cadastro/Requisicao/ConfirmaTokenRequest.kt
+++ app/src/main/java/br/com/visaogrupo/tudofarmarep/Repository/Model/Cadastro/Requisicao/ConfirmaTokenRequest.kt
@@ -1,9 +1,16 @@
 package br.com.visaogrupo.tudofarmarep.Repository.Model.Cadastro.Requisicao
 
+
+
 data class ConfirmaTokenRequest (
     val Celular:String,
     val Token:String,
     val CNPJ:String,
-    val DeviceToken:String
+    val DeviceToken:String,
+    val Plataforma:String,
+    val UDID:String,
+    val VersaoApp:String,
+    val Dispositivo:String,
+    val SistemaOperacional:String
 
 )
